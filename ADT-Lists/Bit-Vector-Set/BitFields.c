69ef14719dad0a3874b50e9e57621ae27ab99684
Crass_Crest_Chris_Cross_Crust <serenochrisclyde@gmail.com>
2025-09-30 13:44:44 +0800

    Create BitFields.c

---
+++ ADT-Lists/Bit-Vector-Set/BitFields.c
@@ -0,0 +1,9 @@
+void initialize(Set *set);
+void insert(Set *set, int element);
+void delete(Set *set, int element);
+bool find(Set set, int element);
+Set union(Set A, Set B);
+Set intersection(Set A, Set B);
+Set difference(Set A, Set B);
+void display(unsigned char set);
+
